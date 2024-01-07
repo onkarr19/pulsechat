0822c703a09ae22e329a438ff621477efc034f98
onkar <onkar.chendage@gmail.com>
2024-01-08 01:56:41 +0530

    add: root api handler

---
+++ backend/user/api/handler.go
@@ -0,0 +1,15 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterRoutes(r *gin.Engine) {
+	r.GET("/", homeHandler)
+}
+
+func homeHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Welcome to PulseChat User Service!",
+	})
+}
